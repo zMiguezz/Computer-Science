eb3b66e379104792b978ea3c4d3df459974ab7b5
Migue <hector_miguel_moren0_gonzalez@outlook.com>
2023-06-17 23:31:05 -0600

    clg & variables

--- 01_OOP/alumno.js
+++ 01_OOP/alumno.js
@@ -6,6 +6,7 @@ class Alumno {
         this.inscrito = inscrito;
     }
 
+
     ObtenerInicialesDeAlumno() {
         const iniciales = this.nombre.charAt(0) + this.apellido.charAt(0);
         console.log(iniciales);
@@ -16,14 +17,31 @@ class Alumno {
     }
 
     aprobado() {
-        const BoolAprobado = if 
+        
     }
 }
 
-let alumnno1 = new Alumno("","",0,true)
+let alumno1 = new Alumno("Luis Fernando", "Luna Martinez", 6, boolAprobado());
+let alumno2 = new Alumno("Maria Luisa", "Perez Ramirez", 5, boolAprobado());
+let alumno3 = new Alumno("Sandra Melissa", "Cuevas Reyes", 9, boolAprobado());
+let alumno4 = new Alumno("Santiago", "Liceo Hernandez", 3, boolAprobado());
+
+console.log(alumno1)
+console.log(alumno2)
+console.log(alumno3)
+console.log(alumno4)
 
 
 
+
+
+    function boolAprobado() {
+        if (this.califFinal > 6) {
+            return true;
+        } else {
+            return false;
+        }
+   }
 
 
 
