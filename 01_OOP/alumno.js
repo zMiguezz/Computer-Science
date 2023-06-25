948e4c309770790d0ef292078e82bef44ca14bdb
Migue <hector_miguel_moren0_gonzalez@outlook.com>
2023-06-24 18:18:00 -0600

    changes fully

--- 01_OOP/alumno.js
+++ 01_OOP/alumno.js
@@ -1,11 +1,10 @@
 class Alumno {
-    constructor(nombre, apellido, califFinal, inscrito) {
+    constructor(nombre, apellido, califFinal) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.califFinal = califFinal;
-        this.inscrito = inscrito;
+        this.inscrito = this.aprobado(); // Asignar resultado de aprobado() a inscrito
     }
-
 
     ObtenerInicialesDeAlumno() {
         const iniciales = this.nombre.charAt(0) + this.apellido.charAt(0);
@@ -17,34 +16,23 @@ class Alumno {
     }
 
     aprobado() {
-        
-    }
-}
-
-let alumno1 = new Alumno("Luis Fernando", "Luna Martinez", 6, boolAprobado());
-let alumno2 = new Alumno("Maria Luisa", "Perez Ramirez", 5, boolAprobado());
-let alumno3 = new Alumno("Sandra Melissa", "Cuevas Reyes", 9, boolAprobado());
-let alumno4 = new Alumno("Santiago", "Liceo Hernandez", 3, boolAprobado());
-
-console.log(alumno1)
-console.log(alumno2)
-console.log(alumno3)
-console.log(alumno4)
-
-
-
-
-
-    function boolAprobado() {
-        if (this.califFinal > 6) {
+        if (this.califFinal > 5) {
             return true;
         } else {
             return false;
         }
-   }
+    }
+}
 
+let alumno1 = new Alumno("Luis Fernando", "Luna Martinez", 7);
+let alumno2 = new Alumno("Maria Luisa", "Perez Ramirez", 5);
+let alumno3 = new Alumno("Sandra Melissa", "Cuevas Reyes", 9);
+let alumno4 = new Alumno("Santiago", "Liceo Hernandez", 3);
 
-
+console.log(alumno1);
+console.log(alumno2);
+console.log(alumno3);
+console.log(alumno4);
 
 
 
